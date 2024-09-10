1f71488643b34e8bd08121f81b45bd1c12fc81c2
Matthew-Innes <innesmk405@gmail.com>
2024-09-10 17:00:18 -0400

    First message

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "Matthew-Innes" ;
+    char STUDENT_ID[] = "mki5164" ;
 
     return 0 ;
 }
